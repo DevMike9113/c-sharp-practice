e17e6392f22890b3a17446a25850a80038b3dfc1
Micheal Skinner <85367897+DevMike9113@users.noreply.github.com>
2022-06-03 15:37:41 -0400

    Add files via upload

---
+++ newApp/Program.cs
@@ -0,0 +1,22 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// // Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace newApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+var isOpen = false;
+
+if (isOpen == true)
+{
+    Console.WriteLine("Yay");
+} else {
+    Console.WriteLine("Ah Man!");
+}
+        }
+    }
+}
